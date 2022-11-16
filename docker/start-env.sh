01fdbc87198f0e98ef601589b76431926bd00685
Viktor Ivanov <viktprog@gmail.com>
2022-11-16 18:20:04 +0900

    Add opencl support inside docker container

--- docker/start-env.sh
+++ docker/start-env.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-exec docker run --rm -u $(id -u):$(id -g) --gpus all --net=host -it -v /mnt:/mnt -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v $(pwd):/workspace -w /workspace -e HOME=/workspace -e SHELL=bash viktprog/cuda-env
+exec docker run --rm --name cuda-env -u $(id -u):$(id -g) --gpus all --net=host -it -v /mnt:/mnt -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v $(pwd):/workspace -w /workspace -e HOME=/workspace -e SHELL=bash viktprog/cuda-env
