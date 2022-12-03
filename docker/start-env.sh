d27cee1f88711a7afd7a297ce35211ebedd87ec8
Viktor Ivanov <viktprog@gmail.com>
2022-12-03 16:34:56 +0900

    Update docker environment

--- docker/start-env.sh
+++ docker/start-env.sh
@@ -1,3 +1,26 @@
 #!/bin/bash
 
-exec docker run --rm --name cuda-env -u $(id -u):$(id -g) --gpus all --net=host -it -v /mnt:/mnt -v /etc/passwd:/etc/passwd:ro -v /etc/group:/etc/group:ro -v $(pwd):/workspace -w /workspace -e HOME=/workspace -e SHELL=bash viktprog/cuda-env
+exec docker run \
+    --rm \
+    --privileged \
+    --name cuda-env \
+    -u $(id -u):$(id --group) \
+    --group-add $(getent group sudo | cut -d: -f3) \
+    --group-add $(getent group docker | cut -d: -f3) \
+    --gpus all \
+    --net=host \
+    -it \
+    -v /mnt:/mnt \
+    -v /media:/media \
+    -v /etc/passwd:/etc/passwd:ro \
+    -v /etc/group:/etc/group:ro \
+    -v /etc/shadow:/etc/shadow:ro \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -v $(pwd):/workspace \
+    -w /workspace \
+    -e HOME=/workspace \
+    -e SHELL=bash \
+    -e TERM \
+    -e DISPLAY \
+    $* \
+    viktprog/cuda-env
