b66bb40dd60940888baab687bb465c5fce22dcc0
Viktor Ivanov <viktprog@gmail.com>
2023-06-04 00:57:59 +0900

    Update scripts

--- docker/init-workspace.sh
+++ docker/init-workspace.sh
@@ -8,9 +8,11 @@ echo "PS1=[cuda]\$PS1" >> .bashrc
 
 python3 -m venv venv --system-site-packages
 source venv/bin/activate
-pip install --upgrade pip setuptools wheel six
+pip install --no-cache --upgrade pip setuptools wheel six
+pip install pycuda
 echo "export PATH=/workspace/venv/bin:\$PATH" >> .profile
 
 git clone https://github.com/microsoft/vcpkg --depth 1
 ./vcpkg/bootstrap-vcpkg.sh --disableMetrics
-
+echo "export VCPKG_FORCE_SYSTEM_BINARIES=1" >> .profile
+echo "export VCPKG_OVERLAY_PORTS=/workspace/wsl-cuda-env/vcpkg-overlay-ports" >> .profile
