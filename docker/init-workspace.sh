4cb9fd0bdef4f0b5cdd683dbe2f28e5959313967
Viktor Ivanov <viktprog@gmail.com>
2024-01-07 18:59:27 +0900

    Add more linters

--- docker/init-workspace.sh
+++ docker/init-workspace.sh
@@ -9,7 +9,7 @@ echo "PS1=[cuda]\$PS1" >> .bashrc
 python3 -m venv venv --system-site-packages
 source venv/bin/activate
 pip install --no-cache --upgrade pip setuptools wheel six
-pip install pycuda
+pip install pycuda pylint flake8 autopep8 pydocstyle cpplint
 echo "export PATH=/workspace/venv/bin:\$PATH" >> .profile
 
 git clone https://github.com/microsoft/vcpkg --depth 1
